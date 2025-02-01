fb9d19b3dbfc98c7fecf8e55511ffff610d264ca
Titus Emmanuel <titusemmanuel3662@gmail.com>
2025-02-01 01:04:54 +0100

    Your Commit

---
+++ Data/Migrations/MigrationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Data.SqlClient;
+
+namespace EmployeesManagement.Data.Migrations
+{
+    public static class MigrationExtensions
+    {
+        public static bool TableExists(this MigrationBuilder migrationBuilder, string tableName)
+        {
+            var sql = $"SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tableName}'";
+            using (var connection = new SqlConnection(migrationBuilder.ActiveProvider))
+            {
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    return command.ExecuteScalar() != null;
+                }
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesManagement.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Department { get; set; }
+        public string Designation { get; set; }
+    }
+}
